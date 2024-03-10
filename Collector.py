83f0506df2b6117afd516bfaa7bc314441dda15a
ReubenkThomas <reubenkthomas@gmail.com>
2024-03-10 15:58:20 -0700

    Phase 1 Corrections v1

--- Collector.py
+++ Collector.py
@@ -20,4 +20,44 @@ class BCI:
 		self.streaming_software = self.BCI_params["streaming_software"]
 		self.streaming_protocol = self.BCI_params["streaming_protocol"]
 		self.cache_size = self.BCI_params["cache_size"]
-		self.cache = ... # TODO: create a NumPy array with 
+		self.cache = ... # TODO: create a NumPy array with size = cache_size
+
+
+class PreProcessingSubBlock:
+	pass
+
+class PostProcessingSubBlock:
+	pass
+
+class MovingAverageFilter(PreProcessingSubBlock):
+	def __init__(self, kernel_size=8, channel_count=4):
+		self.kernel_size = kernel_size
+		self.channel_count = channel_count
+
+	def start(self, stream):
+		... #TODO implement moving average filter
+
+class OutputBlock:
+	pass
+
+
+class ProcessingPipeline:
+	def __init__(self, bci:BCI, **args):
+		self.board = bci
+		self.PreProcessingBlock = []
+		self.PostProcessingBlock = []
+		for arg in args:
+			if arg is PreProcessingSubBlock:
+				self.PreProcessingBlock.append(arg)
+			elif arg is PostProcessingSubBlock:
+				self.PostProcessingBlock.append(arg)
+			else:
+				raise TypeError("Argument is not a processing subblock")
+				
+	def run(self):
+		...
+		'''
+		Processing pipeline should maintain ordering. All preprocessing blocks should run
+		  first in order and store the results of preprocessing into the cache in the BCI object.
+		  Post processing will operate on cache blocks (for Phase 3)
+		'''
